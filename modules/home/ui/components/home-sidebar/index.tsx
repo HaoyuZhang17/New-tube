cb7358b18ce0dab0f1a52e474e5140b83fa377c5
HaoyuZhang17 <156087337+HaoyuZhang17@users.noreply.github.com>
2026-02-26 15:43:34 -0700

    add one separator under the mainsection

--- modules/home/ui/components/home-sidebar/index.tsx
+++ modules/home/ui/components/home-sidebar/index.tsx
@@ -1,11 +1,15 @@
 import { Sidebar, SidebarContent } from "@/components/ui/sidebar";
 import { MainSection } from "./main-section";
+import { Separator } from "@/components/ui/separator";
 
 
 const HomeSidebar = () => {
     return (
         <Sidebar className="pt-16 z-40 border-none">
             <SidebarContent className="bg-background">
+                <MainSection />
+                //这里只是一条横线
+                <Separator />
                 <MainSection />
             </SidebarContent>
         </Sidebar>
