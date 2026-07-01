480a606f3567265b58dcd964eb4d88f8fe78975f
HaoyuZhang17 <156087337+HaoyuZhang17@users.noreply.github.com>
2026-07-01 11:03:29 -0600

    learn how to use middleware to protect router

---
+++ app/(home)/protected/something/page.tsx
@@ -0,0 +1,11 @@
+const Page = () => {
+
+return (
+    <div>
+        Only logged in users should see this!
+    </div>
+)
+
+}
+
+export default Page;
--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,23 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+// 门卫作用，在所有页面之前统一处理
+import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+// createRouteMatcher可以理解成判断器
+  // /protected路由需要保护
+const isProtectedRoute = createRouteMatcher([
+  // 如果只写/protected，那就只保护这个路由；
+    //加上(.*),便可以保护包括这个路由已经以下的所有子路由
+  "/protected(.*)",
+]);
 
-export default clerkMiddleware();
+//所有请求会先来到这；先检查是否是判断器里的路由，如果是，保护这个页面
+  //auth.protect()是clerk提供的方法，检查是否登录；
+    // 已登录：直接访问page.tsx;
+    // 未登录：重定向到登录页或触发 Clerk 登录流程
+// async：允许在函数中使用 await。
+// (auth, req) 是这个回调函数的参数。
+// Clerk 在收到请求后，会调用这个回调，并自动把 auth（认证对象）和 req（请求对象）传进来。
+export default clerkMiddleware(async (auth, req) => {
+  if (isProtectedRoute(req)) await auth.protect();
+});
 
 export const config = {
   matcher: [
